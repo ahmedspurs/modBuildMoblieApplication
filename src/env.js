a24c3920af814aae52b4fc063d27ebdd08544c6e
kola192 <mutwakil96@gmail.com>
2023-06-09 19:37:44 +0200

    preparation for build

---
+++ src/env.js
@@ -0,0 +1,9 @@
+const env = {
+  WP_BASE_URL: "https://eng-alzubair.com/wp-json",
+  WP_USER_NAME: "madda.technology",
+  WP_PASSWORD: "madaa@2023.pass",
+  WP_KEY: "ck_545264083fcc3397b96a88eb1b4991e8dd2647cf",
+  WP_SECRET: "cs_b3af2f6c976abc59acc259f8930ef788ac1b71d3",
+};
+
+export default env;
